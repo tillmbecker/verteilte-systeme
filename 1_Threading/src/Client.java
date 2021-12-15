07bf5155058fc26c2c3131fcffa1492fc5431a0d
Till Becker <tillmbecker@googlemail.com>
2021-12-15 18:17:35 +0100

    Add function createRSA()

--- 1_Threading/src/Client.java
+++ 1_Threading/src/Client.java
@@ -1,8 +1,13 @@
+import eu.boxwork.dhbw.examhelpers.rsa.RSAHelper;
+
+import java.io.File;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * This class implements java socket client
@@ -10,7 +15,6 @@ import java.time.Instant;
  *
  */
 public class Client {
-
     private String host;
     private int port;
     private Socket socket;
@@ -76,10 +80,69 @@ public class Client {
 //        TimeUnit.SECONDS.sleep(5);
 
 //        requestLastMessage();
+        createRSA(100);
         disconnect();
 //        closeServer();
 
 //        ToDo: Die Streams schließen bringt das Programm zum Absturz, obwohl der Server schon geschlossen wurde
+    }
+
+    public void createRSA(int amountOfPrimes) throws IOException, ClassNotFoundException {
+        String publicKey = "";
+        String chiffre = "";
+
+        switch (amountOfPrimes) {
+            case 100:
+                publicKey = "298874689697528581074572362022003292763";
+                chiffre = "b4820013b07bf8513ee59a905039fb631203c8b38ca3d59b475b4e4e092d3979";
+                break;
+            case 1000:
+                publicKey = "249488851623337787855631201847950907117";
+                chiffre = "55708f0326a16870b299f913984922c7b5b37725ce0f6670d963adc0dc3451c8";
+                break;
+            case 10000:
+                publicKey = "237023640130486964288372516117459992717";
+                chiffre = "a9fc180908ad5f60556fa42b3f76e30f48bcddfad906f312b6ca429f25cebbd0";
+                break;
+            case 100000:
+                publicKey = "174351747363332207690026372465051206619";
+                chiffre = "80f7b3b84e8354b36386c6833fe5c113445ce74cd30a21236a5c70f5fdca7208";
+                break;
+            default:
+                System.out.println("Amount of primes not set correctly");
+                break;
+        }
+
+        RSAHelper helper = new RSAHelper();
+
+//        for (String p: fileContents) {
+//            for (String q : fileContents) {
+//                if(helper.isValid(p,q,publicKey)) {
+//                    System.out.println("P/Q fit to the public key: "+helper.isValid(p,q,publicKey));
+//                    System.out.println("Decrypted text is: "+helper.decrypt(p,q,chiffre));
+//                }
+//            }
+//        }
+
+        Message outgoingMessage = new Message();
+
+        // Outgoing message list
+        List<String> messagePayload = new ArrayList<String>();
+        messagePayload.add(Integer.toString(amountOfPrimes));
+        messagePayload.add(chiffre);
+        messagePayload.add(publicKey);
+
+        // Fill outgoingMessage with content
+        outgoingMessage.setReceiver("Server");
+        outgoingMessage.setSender(messageSender);
+        outgoingMessage.setTime(Instant.now());
+        outgoingMessage.setPayload(messagePayload);
+        outgoingMessage.setType("rsa");
+
+        objectOutputStream.writeObject(outgoingMessage);
+        objectOutputStream.flush();
+
+        Message lastMessage = (Message) objectInputStream.readObject();
     }
 
     public void requestLastMessage() throws IOException, ClassNotFoundException {
